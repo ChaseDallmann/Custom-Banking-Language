d2ce5444f482b0c670c04dd52bc9242456b05edc
ChaseDallmann <chasedallmann@comcast.net>
2024-04-28 11:55:22 -0500

    Fixed infinite loop

--- shell.py
+++ shell.py
@@ -11,16 +11,17 @@ import Interpreter
 #The menu that will determine how the input is read
 while True:
     option = input("Enter '1' to enter a command, '2' to read commands from a file: ")
-    while option != '': #Making sure the user enters something
-        if option == '1': #Getting the input from a manual entry in the console
-            text = input("Enter a Token: ")
-            if not text.endswith('\n'):
-                text += '\n'
-        elif option == '2': #Getting the input from bankinginput.txt to read from
-            with open('bankinginput.txt', 'r') as file:
-                lines = file.readlines()
-            text = ''.join(line.rstrip() + '\n' for line in lines)
-        result, error = Lexer.run(text) #Getting tokens from the Lexer
-        parser = Parser.Parser(result) #Creating the parser and passing through the tokens
-        astList = parser.parse() #Generating the AST from the paser
-        Interpreter.Interpreter(astList).interpret() #Banking logic that takes the AST and preforms operations
+    if option == '1': #Getting the input from a manual entry in the console
+        text = input("Enter a Token: ")
+        if not text.endswith('\n'):
+            text += '\n'
+    elif option == '2': #Getting the input from bankinginput.txt to read from
+        with open('bankinginput.txt', 'r') as file:
+            lines = file.readlines()
+        text = ''.join(line.rstrip() + '\n' for line in lines)
+    else:
+        continue
+    result, error = Lexer.run(text) #Getting tokens from the Lexer
+    parser = Parser.Parser(result) #Creating the parser and passing through the tokens
+    astList = parser.parse() #Generating the AST from the paser
+    Interpreter.Interpreter(astList).interpret() #Banking logic that takes the AST and preforms operations
